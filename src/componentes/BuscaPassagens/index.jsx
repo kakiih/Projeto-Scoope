fa9d573d44adbc03be729b9d70a6d0eb94508eb3
kakiih <miguelfialho1234@gmail.com>
2024-09-28 11:27:30 -0300

    Novo Commit

--- src/componentes/BuscaPassagens/index.jsx
+++ src/componentes/BuscaPassagens/index.jsx
@@ -28,135 +28,145 @@ function BuscaViagens() {
   const gradientCustom4 = {
     background: 'linear-gradient(to right, #3b9ae7f8, #0b618b)',
     border: 'none',
-    padding: '10px 20px',  // Aumenta o padding do botão
-    fontSize: '18px'       // Aumenta o tamanho da fonte
+    padding: '10px 20px', 
+    fontSize: '18px'
   };
 
   return (
     <div className="container mt-5">
-      <h2 className="mb-4">Buscar Viagens</h2>
-      <Form onSubmit={handleSubmit}>
-        <Row className="justify-content-center">
-          <Col md={6} className="mb-3">
-            <Form.Group controlId="formOrigem">
-              <Form.Label>Saída de:</Form.Label>
-              <Form.Control
-                type="text"
-                name="origem"
-                value={formData.origem}
-                onChange={handleChange}
-                placeholder="Cidade ou aeroporto de origem"
-                required
-              />
-            </Form.Group>
-          </Col>
-          <Col md={6} className="mb-3">
-            <Form.Group controlId="formDestino">
-              <Form.Label>Destino</Form.Label>
-              <Form.Control
-                type="text"
-                name="destino"
-                value={formData.destino}
-                onChange={handleChange}
-                placeholder="Cidade ou aeroporto de destino"
-                required
-              />
-            </Form.Group>
-          </Col>
-        </Row>
-        <Row className="justify-content-center">
-          <Col md={6} className="mb-3">
-            <Form.Group controlId="formDataPartida">
-              <Form.Label>Data de Partida</Form.Label>
-              <Form.Control
-                type="date"
-                name="dataPartida"
-                value={formData.dataPartida}
-                onChange={handleChange}
-                required
-              />
-            </Form.Group>
-          </Col>
-          <Col md={6} className="mb-3">
-            <Form.Group controlId="formDataRetorno">
-              <Form.Label>Data de Retorno</Form.Label>
-              <Form.Control
-                type="date"
-                name="dataRetorno"
-                value={formData.dataRetorno}
-                onChange={handleChange}
-                required
-              />
-            </Form.Group>
-          </Col>
-        </Row>
-        <Row className="justify-content-center">
-          <Col md={6} className="mb-3">
-            <Form.Group controlId="formNumPassageiros">
-              <Form.Label>Número de Passageiros</Form.Label>
-              <Form.Control
-                type="number"
-                name="numPassageiros"
-                value={formData.numPassageiros}
-                onChange={handleChange}
-                min="1"
-                max="10"
-                required
-              />
-            </Form.Group>
-          </Col>
-          <Col md={6} className="mb-3">
-            <Form.Group controlId="formClasse">
-              <Form.Label>Classe</Form.Label>
-              <Form.Control
-                as="select"
-                name="classe"
-                value={formData.classe}
-                onChange={handleChange}
-                required
-              >
-                <option value="Econômica">Econômica</option>
-                <option value="Executiva">Executiva</option>
-                <option value="Primeira Classe">Primeira Classe</option>
-              </Form.Control>
-            </Form.Group>
-          </Col>
-        </Row>
-        <Row className="justify-content-center">
-          <Col md={6} className="mb-3">
-            <Form.Group controlId="formTipoViagem">
-              <Form.Label>Tipo de Viagem</Form.Label>
-              <Form.Control
-                as="select"
-                name="tipoViagem"
-                value={formData.tipoViagem}
-                onChange={handleChange}
-                required
-              >
-                <option value="Ida e Volta">Ida e Volta</option>
-                <option value="Somente Ida">Somente Ida</option>
-              </Form.Control>
-            </Form.Group>
-          </Col>
-          <Col md={6} className="mb-3">
-            <Form.Group controlId="formPreferencias">
-              <Form.Label>Preferências Adicionais</Form.Label>
-              <Form.Control
-                type="text"
-                name="preferencias"
-                value={formData.preferencias}
-                onChange={handleChange}
-                placeholder="Ex.: Tipo de acomodação, refeições especiais, etc."
-              />
-            </Form.Group>
-          </Col>
-        </Row>
-        <div className="">
-          <Button type="submit" style={gradientCustom4}>
-            Buscar
-          </Button>
-        </div>
-      </Form>
+      <h2 className="mb-4 text-center">Buscar Viagens</h2>
+      <div className="form-container">
+        <Form onSubmit={handleSubmit}>
+          <Row className="justify-content-center">
+            <Col md={6} className="mb-3">
+              <Form.Group controlId="formOrigem">
+                <Form.Label>Saída de:</Form.Label>
+                <Form.Control
+                  type="text"
+                  name="origem"
+                  value={formData.origem}
+                  onChange={handleChange}
+                  placeholder="Cidade ou aeroporto de origem"
+                  required
+                  className="rounded-input"
+                />
+              </Form.Group>
+            </Col>
+            <Col md={6} className="mb-3">
+              <Form.Group controlId="formDestino">
+                <Form.Label>Destino</Form.Label>
+                <Form.Control
+                  type="text"
+                  name="destino"
+                  value={formData.destino}
+                  onChange={handleChange}
+                  placeholder="Cidade ou aeroporto de destino"
+                  required
+                  className="rounded-input"
+                />
+              </Form.Group>
+            </Col>
+          </Row>
+          <Row className="justify-content-center">
+            <Col md={6} className="mb-3">
+              <Form.Group controlId="formDataPartida">
+                <Form.Label>Data de Partida</Form.Label>
+                <Form.Control
+                  type="date"
+                  name="dataPartida"
+                  value={formData.dataPartida}
+                  onChange={handleChange}
+                  required
+                  className="rounded-input"
+                />
+              </Form.Group>
+            </Col>
+            <Col md={6} className="mb-3">
+              <Form.Group controlId="formDataRetorno">
+                <Form.Label>Data de Retorno</Form.Label>
+                <Form.Control
+                  type="date"
+                  name="dataRetorno"
+                  value={formData.dataRetorno}
+                  onChange={handleChange}
+                  required
+                  className="rounded-input"
+                />
+              </Form.Group>
+            </Col>
+          </Row>
+          <Row className="justify-content-center">
+            <Col md={6} className="mb-3">
+              <Form.Group controlId="formNumPassageiros">
+                <Form.Label>Número de Passageiros</Form.Label>
+                <Form.Control
+                  type="number"
+                  name="numPassageiros"
+                  value={formData.numPassageiros}
+                  onChange={handleChange}
+                  min="1"
+                  max="10"
+                  required
+                  className="rounded-input"
+                />
+              </Form.Group>
+            </Col>
+            <Col md={6} className="mb-3">
+              <Form.Group controlId="formClasse">
+                <Form.Label>Classe</Form.Label>
+                <Form.Control
+                  as="select"
+                  name="classe"
+                  value={formData.classe}
+                  onChange={handleChange}
+                  required
+                  className="rounded-input"
+                >
+                  <option value="Econômica">Econômica</option>
+                  <option value="Executiva">Executiva</option>
+                  <option value="Primeira Classe">Primeira Classe</option>
+                </Form.Control>
+              </Form.Group>
+            </Col>
+          </Row>
+          <Row className="justify-content-center">
+            <Col md={6} className="mb-3">
+              <Form.Group controlId="formTipoViagem">
+                <Form.Label>Tipo de Viagem</Form.Label>
+                <Form.Control
+                  as="select"
+                  name="tipoViagem"
+                  value={formData.tipoViagem}
+                  onChange={handleChange}
+                  required
+                  className="rounded-input"
+                >
+                  <option value="Ida e Volta">Ida e Volta</option>
+                  <option value="Somente Ida">Somente Ida</option>
+                </Form.Control>
+              </Form.Group>
+            </Col>
+            <Col md={6} className="mb-3">
+              <Form.Group controlId="formPreferencias">
+                <Form.Label>Preferências Adicionais</Form.Label>
+                <Form.Control
+                  type="text"
+                  name="preferencias"
+                  value={formData.preferencias}
+                  onChange={handleChange}
+                  placeholder="Ex.: Tipo de acomodação, refeições especiais, etc."
+                  className="rounded-input"
+                />
+              </Form.Group>
+            </Col>
+          </Row>
+          <div className="text-center">
+            <Button type="submit" style={gradientCustom4}>
+              Buscar
+            </Button>
+          </div>
+        </Form>
+      </div>
     </div>
   );
 }
--- src/componentes/NavBar/index.jsx
+++ src/componentes/NavBar/index.jsx
@@ -1,68 +1,115 @@
+import React, { useRef } from 'react';
 import { Link } from 'react-router-dom';
-import { Navbar } from 'react-bootstrap';
-import React, { useRef, useEffect } from 'react';
+import 'bootstrap/dist/js/bootstrap.bundle.min'; // Certifique-se de que o JS do Bootstrap está importado
 
 function NavBar() {
   const navbarRef = useRef(null);
 
-  useEffect(() => {
-    const handleClickOutside = (event) => {
-      if (navbarRef.current && !navbarRef.current.contains(event.target)) {
-        const bsCollapse = new window.bootstrap.Collapse(navbarRef.current, {
-          toggle: false
-        });
-        bsCollapse.hide();
-      }
-    };
-
-    document.addEventListener('mousedown', handleClickOutside);
-    return () => document.removeEventListener('mousedown', handleClickOutside);
-  }, []);
-  const gradientCustom4 = {
-    background: 'linear-gradient(to right, #21468bf8, #3390be)'
+  const toggleNavbar = () => {
+    const navbar = navbarRef.current;
+    if (navbar.classList.contains('show')) {
+      // Fecha a navbar se estiver aberta
+      const bsCollapse = new window.bootstrap.Collapse(navbar, {
+        toggle: false
+      });
+      bsCollapse.hide();
+    } else {
+      // Abre a navbar se estiver fechada
+      const bsCollapse = new window.bootstrap.Collapse(navbar, {
+        toggle: false
+      });
+      bsCollapse.show();
+    }
   };
 
   return (
-    <nav className="navbar navbar-expand-sm navbar-dark py-3" style={gradientCustom4}>
+    <nav className="navbar navbar-expand-lg navbar-dark" style={{backgroundColor: '#253670'}}> 
       <div className="container-fluid">
-      <Navbar.Brand as={Link} to="/">
-        <img
-          src="https://i.imgur.com/qlXlrFc.png" // Caminho para a imagem da logomarca
-          alt="Logomarca"
-          style={{ height: '50px' }} // Ajuste a altura conforme necessário
-        />
-      </Navbar.Brand>
+        {/* Botão de toggle para dispositivos menores */}
         <button
-        className="navbar-toggler"
-        type="button"
-        data-bs-toggle="collapse"
-        data-bs-target="#mynavbar"
-        aria-controls="mynavbar"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
+          className="navbar-toggler"
+          type="button"
+          onClick={toggleNavbar}
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
         >
           <span className="navbar-toggler-icon"></span>
         </button>
 
-        <div className="collapse navbar-collapse" id="mynavbar" ref={navbarRef}>
-          <ul className="navbar-nav me-auto">
-          <li className="nav-item">
-              <Link className="nav-link" to="/">Inicio</Link>
+        {/* Logo */}
+        <Link className="navbar-brand" to="/" style={{ color: 'white' }}> {/* Texto Branco */}
+          <img
+            src="https://i.imgur.com/qlXlrFc.png"
+            alt="Logomarca"
+            style={{ height: '50px' }}
+          />
+        </Link>
+
+        {/* Itens do navbar que colapsam */}
+        <div className="collapse navbar-collapse" id="navbarSupportedContent" ref={navbarRef}>
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <Link className="nav-link" to="/" style={{ color: 'white' }}>Início</Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to="/sobre">Sobre nós</Link>
+              <Link className="nav-link" to="/pacotes" style={{ color: 'white' }}>Pacotes</Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to="/contato">Contato</Link>
+              <Link className="nav-link" to="/destinos" style={{ color: 'white' }}>Destinos</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/sobre" style={{ color: 'white' }}>Sobre nós</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/contato" style={{ color: 'white' }}>Contato</Link>
             </li>
           </ul>
-          <form className="d-flex">
-            <Link to="/sign">
-            <div className="d-flex justify-content-center align-items-center rounded-circle border border-white" style={{ width: "40px", height: "40px" }}> 
-              <i className="fas fa-user text-white" style={{ fontSize: "20px" }}></i>
+
+          {/* Ícones de carrinho, notificações e perfil */}
+          <div className="d-flex align-items-center gap-3">
+            <Link className="link-secondary" to="/carrinho" style={{ color: 'white' }}>
+              <i className="fas fa-shopping-cart"></i>
+            </Link>
+
+            <div className="dropdown">
+              <a
+                data-mdb-dropdown-init
+                className="link-secondary dropdown-toggle hidden-arrow"
+                href="/"
+                id="navbarDropdownMenuLink"
+                role="button"
+                aria-expanded="false"
+                style={{ color: 'white' }}
+              >
+                <i className="fas fa-bell"></i>
+                <span className="badge rounded-pill badge-notification bg-danger">1</span>
+              </a>
+              <ul className="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdownMenuLink">
+                <li><a className="dropdown-item" href="/">Novidades</a></li>
+                <li><a className="dropdown-item" href="/">Ofertas especiais</a></li>
+                <li><a className="dropdown-item" href="/">Ajuda</a></li>
+              </ul>
             </div>
-            </Link>               
-          </form>
+
+            {/* Avatar do usuário que redireciona para a página de sign up */}
+            <div className="dropdown">
+              <Link
+                className="dropdown-toggle d-flex align-items-center hidden-arrow"
+                to="/sign" // Agora o avatar redireciona para Sign Up
+              >
+                  <div className="link-secondary" style={{ color: 'white' }}>
+                    <i className="fas fa-user"></i>
+                  </div>
+              </Link>
+              <ul className="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdownMenuAvatar">
+                <li><Link className="dropdown-item" to="/perfil">Meu Perfil</Link></li>
+                <li><Link className="dropdown-item" to="/configuracoes">Configurações</Link></li>
+                <li><Link className="dropdown-item" to="/logout">Sair</Link></li>
+              </ul>
+            </div>
+
+          </div>
         </div>
       </div>
     </nav>
